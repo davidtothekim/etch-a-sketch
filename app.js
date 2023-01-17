911945373c16316e6d0f8799b53dd0531b4dca7c
David Kim <dgkim.david@gmail.com>
2023-01-16 23:45:01 -0500

    paint bucket

--- app.js
+++ app.js
@@ -7,14 +7,20 @@ let gridSizeLabel = document.querySelector('.gridSize');
 let gridSizeContainer = document.querySelector('.gridSizeContainer');
 
 let eraserTool = document.querySelector('.toolEraser');
-
+let pencilTool = document.querySelector('.toolPencil');
+let paintTool = document.querySelector('.toolPaint');
 let gridTool = document.querySelector('.toolGrid');
 let clearBttn = document.querySelector('.bttnClear');
+let gridCells = document.querySelectorAll('.gridCell');
 
 clearBttn.onclick = (e) => clearGrid();
 colorPicker.oninput = (e) => setSelectedColor(e.target.value);
 gridSizeSlider.oninput = (e) => drawGrid(e.target.value);
 gridTool.onclick = () => toggleGridSlider();
+eraserTool.onclick = (e) => selectTool(e.target.parentElement, 'eraser');
+pencilTool.onclick = (e) => selectTool(e.target.parentElement, 'pencil');
+paintTool.onclick = (e) => selectTool(e.target.parentElement, 'paint');
+gridTool.onclick = (e) => selectTool(e.target.parentElement, 'grid');
 
 // Variables
 
@@ -22,10 +28,12 @@ let selectedColour = '#ff0000';
 
 let selectedTool = '';
 
-let gridDimension = 16;
+let gridDimension = 2;
 let mouseClicked = false;
 document.body.onmousedown = () => (mouseClicked = true);
 document.body.onmouseup = () => (mouseClicked = false);
+
+const BACKGROUNDCOLOUR = '#e1e7e5';
 
 //Functions
 
@@ -49,7 +57,7 @@ function drawGrid(gridDimension) {
 	gridBox.style.gridTemplateColumns = `repeat(${gridDimension}, 1fr)`;
 	gridBox.style.gridTemplateRows = `repeat(${gridDimension}, 1fr)`;
 
-	for (let i = 0; i <= area; i++) {
+	for (let i = 0; i < area; i++) {
 		let gridCell = drawGridCell();
 		gridBox.appendChild(gridCell);
 	}
@@ -70,19 +78,17 @@ function drawGridCell() {
 // Change colour of grid cells
 
 function changeColor(event) {
-	console.log();
 	if (event.type === 'mouseover' && !mouseClicked) return;
-	event.target.style.backgroundColor = selectedColour;
-}
-
-function changeColor2() {
-	gridBox.style.backgroundColor = 'red';
+	else if (selectedTool === 'eraser') event.target.style.backgroundColor = '#ffffff';
+	else if (selectedTool === 'pencil') event.target.style.backgroundColor = selectedColour;
+	else if (selectedTool === 'paint') paintGrid(selectedColour);
 }
 
 // Clear grid cells
 
 function clearGrid() {
 	let gridCells = document.querySelectorAll('.gridCell');
+	console.log(gridCells);
 	for (let element of gridCells) {
 		element.style.backgroundColor = '#ffffff';
 	}
@@ -95,6 +101,59 @@ function toggleGridSlider() {
 	gridSizeContainer.classList.toggle('hidden');
 }
 
+// Select tool
+
+function selectTool(element, tool) {
+	if (tool === 'grid') toggleGridSlider();
+
+	selectedTool != tool ? (selectedTool = tool) : (selectedTool = '');
+
+	element.classList.toggle('selected');
+}
+
+// Paint grid
+
+function paintGrid(colour) {
+	let gridCells = document.querySelectorAll('.gridCell');
+
+	for (let element of gridCells) {
+		let example = getComputedStyle(element, null);
+		let bColor = example.getPropertyValue('background-color');
+		console.log(bColor);
+		// if (!element.style.backgroundColour) {
+		// 	console.log(element.style.backgroundColor);
+		// 	console.log(element.style.classList);
+		// 	console.log('null');
+		// } else {
+		// 	console.log(element.style.backgroundColor);
+		// }
+	}
+}
+
+// RGBToHex(element.style.backgroundColor) !== colour
+
+// Convert rgb to hex
+
+function RGBToHex(rgb) {
+	// Choose correct separator
+	let sep = rgb.indexOf(',') > -1 ? ',' : ' ';
+	// Turn "rgb(r,g,b)" into [r,g,b]
+	rgb = rgb.substr(4).split(')')[0].split(sep);
+
+	let r = (+rgb[0]).toString(16),
+		g = (+rgb[1]).toString(16),
+		b = (+rgb[2]).toString(16);
+
+	if (r.length == 1) r = '0' + r;
+	if (g.length == 1) g = '0' + g;
+	if (b.length == 1) b = '0' + b;
+
+	let hex = '#' + r + g + b;
+
+	return '#' + r + g + b;
+}
+
+RGBToHex('rgb(60, 52, 52)');
 // drawGrid();
 
 drawGrid(gridDimension);
